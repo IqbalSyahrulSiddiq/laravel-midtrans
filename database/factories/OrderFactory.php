fa02e573a10a0d8da8ee2691802c43ebf6573cb6
Iqbal Syahrul Siddiq <ikbalmuhammad53@gmail.com>
2022-04-16 06:22:21 +0700

    add seeder

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Order;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+$factory->define(Order::class, function (Faker $faker) {
+    return [
+        'number' => $this->faker->randomNumber(8),
+        'total_price' => $this->faker->numberBetween(25000, 200000),
+        'payment_status' => 1,
+    ];
+});
