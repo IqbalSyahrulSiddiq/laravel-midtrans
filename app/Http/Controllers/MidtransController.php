bb45dbe477d235d41f81ee307b866d40467e7d08
Iqbal Syahrul Siddiq <ikbalmuhammad53@gmail.com>
2022-04-18 19:52:02 +0700

    add callback

--- app/Http/Controllers/MidtransController.php
+++ app/Http/Controllers/MidtransController.php
@@ -9,11 +9,22 @@ class MidtransController extends Controller
 {
     public function callbackNotification(Request $request)
     {
-        $pesanSuccess = 'Request berhasil';
-        
+        /* $pesanSuccess = 'Request berhasil';
+
         return response()->json([
             'data' => $pesanSuccess,
             'message' => 'Sukses melakukan request'
-        ], 200);
+        ], 200); */
+
+        $callbackNotification['order_id'] = $request->order_id;
+
+        $updateStatusBayar = DB::table('order')->where('number',$callbackNotification['order_id'])->update([
+            'payment_status' => 2
+        ]);
+
+        return response()->json([
+            'message' => 'Sukses melakukan pembayaran'
+        ], 200); 
+
     }
 }
