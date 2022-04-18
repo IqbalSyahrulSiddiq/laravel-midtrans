335f43dcee0fcd55a284cc44e040e7dc636fed1a
Iqbal Syahrul Siddiq <ikbalmuhammad53@gmail.com>
2022-04-18 21:43:27 +0700

    add filter settlement condition

--- app/Http/Controllers/MidtransController.php
+++ app/Http/Controllers/MidtransController.php
@@ -16,15 +16,26 @@ class MidtransController extends Controller
             'message' => 'Sukses melakukan request'
         ], 200); */
 
+        $callbackNotification['transaction_time'] = $request->transaction_time;
+        $callbackNotification['transaction_status'] = $request->transaction_status;
+        $callbackNotification['transaction_id'] = $request->transaction_id;
         $callbackNotification['order_id'] = $request->order_id;
+        $callbackNotification['status_message'] = $request->status_message;
+        $callbackNotification['status_code'] = $request->status_code;
+        $callbackNotification['signature_key'] = $request->signature_key;
+        $callbackNotification['payment_type'] = $request->payment_type;
+        $callbackNotification['merchant_id'] = $request->merchant_id;
+        $callbackNotification['gross_amount'] = $request->gross_amount;
 
-        $updateStatusBayar = DB::table('orders')->where('number',$callbackNotification['order_id'])->update([
-            'payment_status' => 2
-        ]);
-
-        return response()->json([
-            'message' => 'Sukses melakukan pembayaran'
-        ], 200); 
-
+        if($callbackNotification['payment_status'] == 'settlement')
+        {
+            $updateStatusBayar = DB::table('orders')->where('number',$callbackNotification['order_id'])->update([
+                'payment_status' => 2
+            ]);
+    
+            return response()->json([
+                'message' => 'Sukses melakukan pembayaran'
+            ], 200);
+        }
     }
 }
